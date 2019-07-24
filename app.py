42f72ae1e85c2e4f654e5adcb1d55cd0d1a96446
闫化强 <206936012@qq.com>
2019-07-24 21:28:06 +0800

    restful

---
+++ app.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/restful/api/<string:model_id>/<int:task_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def get_task(model_id, task_id):
+    if request.method == 'GET':
+        return '获取:%s-%s' % (model_id, task_id)
+    if request.method == 'POST':
+        data = request.data
+        return '创建:%s-%s-%s' % (model_id, task_id, data)
+    if request.method == 'PUT':
+        data = request.data
+        return '更新:%s-%s-%s' % (model_id, task_id, data)
+    if request.method == 'DELETE':
+        return '删除:%s-%s' % (model_id, task_id)
+
+
+if __name__ == '__main__':
+    app.run()
